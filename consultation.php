540a1265664cd33100b6ab115f12c2b33316a93f
aliciahumblot <105773688+aliciahumblot@users.noreply.github.com>
2022-06-22 15:20:06 +0200

    Update consultation.php

--- consultation.php
+++ consultation.php
@@ -26,8 +26,33 @@
   </ul>
 </nav>
 <body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
+    <?php
+
+        /* Accès à la base */
+        include ("mysql.php");
+
+        
+        $requete = "SELECT * FROM `mesures` ORDER BY DESC $id_mesure";
+        $resultat = mysqli_query($id_bd, $requete)
+          or die("Execution de la requete impossible : $requete");
+        mysqli_close($id_bd);
+
+        /* Affichage du tableau  */
+        echo '<div class="tablo">';
+        echo '<table>';
+        while($ligne=mysqli_fetch_assoc($resultat))
+         {  
+          extract($ligne);
+          echo '<tr>';
+          echo  "<td> $id_mesure </td>";
+          echo  "<td> $date </td>";
+          echo  "<td> $heure </td>";
+          echo  "<td> $valeur </td>";
+          echo  "<td> $capteur </td>";
+          echo '</tr>';
+         }
+        echo '</table>';
+        echo '</div>';
+    ?>  
 </body>
 </html>
