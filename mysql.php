8a95a01df434cc1e87c332745c659eaa8eb5573e
aliciahumblot <105773688+aliciahumblot@users.noreply.github.com>
2022-06-22 12:40:19 +0200

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -1,6 +1,6 @@
 <?php 
 	/*script of connection to data base SAe23 */
-	$id_bd = mysql_connect('127.0.0.1', 'jballarin', 'rtblagnac', 'SAe23')
+	$id_bd = mysql_connect('127.0.0.1', 'root', 'passroot', 'sae23')
 	or die ("Connexion au serveur et/ou à la base de données impossible");
 
 	mysql_query($id_bd, "SET NAMES 'utf8'");
